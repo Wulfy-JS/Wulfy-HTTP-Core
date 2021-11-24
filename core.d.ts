d29ed8700acbe0f349b95a2910f14040e7edcc00
80LK <80lkru@gmail.com>
2021-11-24 21:48:34 +0300

    rewrite to TypeScript

--- core.d.ts
+++
@@ -1,5 +0,0 @@
-import Core from "red-project";
-
-declare abstract class HttpCore extends Core { }
-
-export default HttpCore;
---
+++ core.ts
@@ -0,0 +1,34 @@
+import Core from "red-project";
+import { createServer, Server } from "http";
+
+declare module "http" {
+	interface IncomingMessage {
+		body: string;
+	}
+}
+
+abstract class HttpCore extends Core {
+	private port: number;
+	private server: Server;
+
+	protected __init() {
+		this.port = this.config.get("port", 80);
+		this.server = createServer();
+		this.server.on("request", (req, res) => {
+			req.body = "";
+			req.on("data", c => req.body += c);
+			req.on("end", () => {
+				this.response(req, res);
+			})
+		})
+	}
+
+	protected async __launch() {
+		this.server.listen(this.port || 80);
+	}
+	protected __shutdown() {
+		this.server.close();
+	}
+}
+
+export default HttpCore;
